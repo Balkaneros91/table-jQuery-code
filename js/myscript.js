dd20e5d6a00d94fc1d76549c091a0c36d9271796
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-02-03 10:21:31 +0000

    Add table

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // $('h2').css('text-decoration', 'underline');    // underline all <h2> elements
+
+    $('h2').addClass('underline');
+
+    $('h2').removeClass('underline');
+
+    // $('ul').css('border', 'solid 1px #ccc');        // adds a border to all lists 
+
+    $('ul').addClass('border');
+
+    $('ul').removeClass('border');
+
+});
